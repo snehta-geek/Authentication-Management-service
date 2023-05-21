0a6fe5a75ff190138f4fe53778d6484d68412a60
snehta-geek <snehtabarua4896@gmail.com>
2023-05-22 01:09:23 +0530

    First commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect("mongodb://localhost:27017/authDB",
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-app.use('/api',userRoute);
+app.use('/api',userRoute); 
 
 
 const port = process.env.PORT || 8080;
